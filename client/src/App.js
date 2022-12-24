41a4eb9f4872fdee854e39eb347ba1dc40908778
nghia3110 <nghiaxu3110@gmail.com>
2022-12-25 02:40:42 +0700

    add routers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { DefaultLayout } from './components/Layout';
+import { publicRoutes } from './router';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={<Layout><Page /></Layout>}>
+              </Route>
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/router/index.js
@@ -0,0 +1,15 @@
+import Home from "../pages/Admin/Home";
+import Productlines from "../pages/Admin/Productlines";
+import Users from "../pages/Admin/Users";
+
+const publicRoutes = [
+    {path: '/', component: Home},
+    {path: '/productlines', component: Productlines},
+    {path: '/users', component: Users}
+];
+
+const privateRoutes = [
+
+];
+
+export {publicRoutes, privateRoutes};
