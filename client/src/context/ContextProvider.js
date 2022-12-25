4a25d191a3e32d7527ab202f292c826d3ccf1200
nghia3110 <nghiaxu3110@gmail.com>
2022-12-25 23:01:38 +0700

    init context

---
+++ client/src/context/ContextProvider.js
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+
+const initialState = {
+    notification: false,
+    userProfile: false
+};
+
+export const ContextProvider = ({ children }) => {
+    const [screenSize, setScreenSize] = useState(undefined);
+    const [currentColor, setCurrentColor] = useState('#03C9D7');
+    const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+  
+    const setColor = (color) => {
+      setCurrentColor(color);
+      localStorage.setItem('colorMode', color);
+    };
+  
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+  
+    return (
+      <StateContext.Provider value={{ currentColor, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setColor}}>
+        {children}
+      </StateContext.Provider>
+    );
+  };
+  
+  export const useStateContext = () => useContext(StateContext);
