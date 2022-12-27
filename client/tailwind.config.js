725d8385724c835f6bab4f746d0bb22bfab55ee2
nghia3110 <nghiaxu3110@gmail.com>
2022-12-27 15:59:36 +0700

    config project

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         'secondary-dark-bg': '#33373E',
         'light-gray': '#F7F7F7',
         'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        'log-bg': 'rgb(240, 242, 245)',
       },
       borderWidth: {
         1: '1px',
